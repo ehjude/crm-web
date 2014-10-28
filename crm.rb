f6ca7ac5a37a5a23d82e8eda0dfd4a2330044178
Jude Fiorillo <jude.fiorillo@gmail.com>
2014-10-28 15:16:14 -0400

    create contact class

--- crm.rb
+++ crm.rb
@@ -1,6 +1,20 @@
 require 'sinatra'
+require './contact' 
+require './rolodex'
+
 
 get '/' do 
 	@crm_app_name = "My CRM"
 	erb :index
 end
+
+get '/contacts' do 
+	@contacts = []
+	@contacts << Contact.new("Jude", "Fiorillo", "boss@youdaboss.com", "Chief Dude")
+
+	erb :contacts
+end
+
+get '/contacts/new' do 
+end
+
